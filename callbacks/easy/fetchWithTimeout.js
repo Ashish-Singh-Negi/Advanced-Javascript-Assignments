79cb2c386fb7aa6314bebfe09f627026cb89df9b
Ashish Singh Negi <ashishamonegi@gmail.com>
2026-02-03 21:13:07 +0530

    solved 2 questions

--- callbacks/easy/fetchWithTimeout.js
+++ callbacks/easy/fetchWithTimeout.js
@@ -5,8 +5,29 @@
 // If the request completes within the specified time, the Promise resolves with the fetched data. 
 // If the operation exceeds the time limit, the Promise rejects with the message "Request Timed Out".
 
-function fetchWithTimeout(url, ms) {}
+function fetchWithTimeout(url, ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("Request Timed Out")
+        }, ms)
 
-function fetchWithTimeoutClean(url, ms) {}
+        fetch(url).then(data => {
+            resolve(data)
+        })
+    })
+}
+
+function fetchWithTimeoutClean(url, ms) {
+    return new Promise((resolve, reject) => {
+        const id = setTimeout(() => {
+            reject("Request Timed Out")
+        }, ms)
+
+        fetch(url).then(data => {
+            resolve(data)
+            clearTimeout(id)
+        })
+    })
+}
 
 module.exports = { fetchWithTimeout, fetchWithTimeoutClean };
