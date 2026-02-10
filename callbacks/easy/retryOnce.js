7f40dddad027f0cb30bf75fb26a2be8188e0988f
Ashish Singh Negi <ashishamonegi@gmail.com>
2026-02-10 12:04:53 +0530

    solve 1 easy callback problem

--- callbacks/easy/retryOnce.js
+++ callbacks/easy/retryOnce.js
@@ -8,6 +8,28 @@
 
 function retryOnce(fn) {
 
+    return async function (...args) {
+        const callback = args.pop()
+        let attempts = 0
+
+        function attempt() {
+            attempts++
+
+            fn((err, success) => {
+                if (success) {
+                    return callback(null, success)
+                }
+
+                if (attempts < 2) {
+                    return attempt()
+                }
+
+                return callback(err, null)
+            })
+        }
+
+        attempt()
+    }
 }
 
 module.exports = retryOnce;
