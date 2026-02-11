0777afd0063952f28bca92c1bdd45b39a755745e
Ashish Singh Negi <ashishamonegi@gmail.com>
2026-02-11 16:52:59 +0530

    solved 1 medium level callback problem

--- callbacks/medium/batchProcess.js
+++ callbacks/medium/batchProcess.js
@@ -13,7 +13,40 @@
 // - Stop and return an error if any task fails.
 
 function batchProcess(items, limit, worker, onComplete) {
+    let nextIndex = 0;
+    let activeProcess = 0;
+    let results = [];
+    let isFinished = false;
 
+    function launchProcess() {
+        if (isFinished || activeProcess >= limit) return;
+
+        if (nextIndex === items.length) {
+            if (activeProcess == 0) {
+                onComplete(null, results);
+            }
+            return
+        }
+
+        const currentIndex = nextIndex;
+        nextIndex++;
+        activeProcess++;
+
+        worker(items[currentIndex], (err, result) => {
+            if (err) {
+                isFinished = true;
+                onComplete(err, result);
+                return
+            }
+
+            results.push(result)
+
+            activeProcess--;
+            launchProcess()
+        })
+    }
+
+    launchProcess()
 }
 
 module.exports = batchProcess;
