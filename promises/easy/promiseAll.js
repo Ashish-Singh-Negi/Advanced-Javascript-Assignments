b98248fac730f035b54e853da79fc4687b72b322
Ashish Singh Negi <ashishamonegi@gmail.com>
2026-02-12 13:47:27 +0530

    solved promiseAll and promiseAny promise problem

--- promises/easy/promiseAll.js
+++ promises/easy/promiseAll.js
@@ -3,6 +3,25 @@
 // You are required to implement your own version of Promise.all without using the built-in method. 
 // The function should accept an array of values that may include Promises or plain constants. 
 // It must resolve with an array of results in the same order once all inputs resolve, or reject immediately if any input rejects.
-function promiseAll(promises) {}
+
+
+
+
+async function promiseAll(promises) {
+    const result = []
+
+    if (!Array.isArray(promises)) throw new TypeError();
+
+    for (const promise of promises) {
+        try {
+            const res = await promise
+            result.push(res)
+        } catch (err) {
+            throw err
+        }
+    }
+
+    return result;
+}
 
 module.exports = promiseAll;
--- promises/easy/promiseAny.js
+++ promises/easy/promiseAny.js
@@ -3,6 +3,29 @@
 // You are required to implement a function named promiseAny that accepts an array of Promises. 
 // The function should return a new Promise that resolves immediately when any one of the input promises resolves successfully. 
 // If all the promises reject, the returned Promise should reject with an error.
-function promiseAny(promises) {}
+function promiseAny(promises) {
+
+    return new Promise((resolve, reject) => {
+        if (!Array.isArray(promises) || promises.length == 0) {
+            reject(new Error("Empty iterable"))
+        }
+
+        let rejectCount = 0;
+
+        for (const promise of promises) {
+            new Promise(async () => {
+                try {
+                    const result = await promise;
+                    resolve(result)
+                } catch (error) {
+                    rejectCount++;
+                    if (rejectCount === promises.length) {
+                        reject(new Error("All promises were rejected"))
+                    }
+                }
+            })
+        }
+    })
+}
 
 module.exports = promiseAny;
